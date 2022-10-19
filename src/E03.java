485d45918b8d912725d50c17882c835eac673757
Ms-Yosa <yosa.jilliancr@gmail.com>
2022-10-19 17:18:09 +0800

    p3 ex

---
+++ src/E03.java
@@ -0,0 +1,61 @@
+import java.util.Scanner;
+
+public class E03 {
+    public static void main (String[] args){
+
+      /*
+
+      = > >> If else
+
+        System.out.println("Pick a number between 1 and 10");
+        Scanner scanner = new Scanner(System.in);
+
+        int inputtedNum = scanner.nextInt();
+
+        if(inputtedNum < 5){
+            System.out.println("Enjoy the good luck a friend brings you!");
+        }else{
+            System.out.println("Your shoe selection will make you happy today");
+        }
+
+       = > >> While Loop
+
+        Scanner input = new Scanner(System.in);
+
+        boolean isOnRepeat = true;
+        while(isOnRepeat){
+            System.out.println("Playing current song");
+            System.out.println("Dili na i-continue? If so, answer yes");
+            String userInput = input.next();
+
+            if(userInput.equals("yes")){
+                isOnRepeat = false;
+            }
+        }
+
+        System.out.println("Playing Next Song");
+
+        = > >> Simple Challenge
+
+        Scanner input = new Scanner(System.in);
+        String A = "Xiao";
+        String B = "Yanfei";
+        String C = "Venti";
+
+
+        System.out.println("What is my dog's name?");
+        System.out.println("A. " + A + " B. " + B + " C. " + C);
+        String userInput = input.next();
+
+      if(userInput.toUpperCase().equals("A") || userInput.equals("B")){
+          System.out.println("Wrong Answer, my friend :(((");
+          System.out.println("The answer is C. Venti");
+      }else{
+          System.out.println("Correct, madam! ");
+      }
+
+
+       */
+
+    }
+}
