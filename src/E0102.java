eff12362d30fcccb7eec4bf2961dffc15a2f673f
Ms-Yosa <yosa.jilliancr@gmail.com>
2022-10-19 16:19:49 +0800

    p1 & p2 ex

--- src/E0102.java
+++ src/E0102.java
@@ -1,7 +1,35 @@
+import java.util.Scanner; // needed for to use Scanner
+
 public class E0102 {
     public static void main(String[] args) {
 
+        /* Notes
+
+        index starts at 0
+        .charAt(index) => a java method used to locate and identify the said char in the given index
+        .Next...()=> a method of Java Scanner class which is used to scan the next token of the input as the given data type.
+         use + to concat two or more strings
+        System.out => output
+        System.in => input
+
+     */
+
+        /* exercised from chap 2
+
         System.out.println("Hello world!");
+        int studentAge = 15;
+        double studentGPA = 3.45;
+        String studentFName = "Jian";
+        String studentLName = "Yosa";
+        boolean hasPerfectAttendance = true;
+
+        System.out.println(studentFName + " " + studentLName + " has a GPA of " + studentGPA);
+        System.out.println("What do you want to update it to?");
+
+        Scanner input = new Scanner(System.in);
+        studentGPA = input.nextDouble();
+        System.out.println(studentFName + " " + studentLName + " now has a GPA of " + studentGPA);
+*/
 
 
     }
