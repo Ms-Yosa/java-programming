61c68296e0ec6a660d7ca6847fce27df31dbda1d
Ms-Yosa <yosa.jilliancr@gmail.com>
2022-10-20 11:38:40 +0800

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -2,4 +2,29 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+
+    /*
+
+    >>> Classes start with an uppercase character
+    public class MyClass
+
+    >>> Methods & variables start with a lowercase character
+    void doSomething(String withThis)
+
+    >>> Constants in java always in uppercase and uses public static final
+     public static final String FIRSTNAME = "David";
+
+     -------------------
+
+     >>> Primitive Data Type
+     . All lowercase ( int, char, short, long, double, float, boolean, byte)
+     . declaration:  datatype identifier assignment value;
+                      int myVar = 5;
+     . alpha notation: use inferred data typing : var identifier assignment value+(f = float, d = double, L = Long)
+                        var myFloat = 5f
+
+     >>>Wrapper Class
+     . Starts with an Uppercase (Integer, Character, Short ...)
+
+     */
 }
