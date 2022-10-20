14724937a16c3d28602183cb0c666abb356b8982
Ms-Yosa <yosa.jilliancr@gmail.com>
2022-10-21 07:58:13 +0800

    convert prmitive values to string

--- src/E01.java
+++ src/E01.java
@@ -1,2 +1,34 @@
+
+
 public class E01 {
+
+    public static void main (String[] args){
+        int result = 10 + 12;
+        String answer =  "The answer is " + result;
+        System.out.println(answer);
+
+        String howMany = 20 + " things";
+        System.out.println(howMany);
+
+        // int to string
+        int intValue = 42;
+        String newString = Integer.toString(intValue);
+        System.out.println(newString);
+
+        //boolean to string
+        boolean boolValue = true;
+        String stringVal = Boolean.toString(boolValue);
+        System.out.println(stringVal);
+
+
+
+        char newStringChar = newString.charAt(1);
+        System.out.println(newStringChar);
+
+        char stringValChar = stringVal.charAt(3);
+        System.out.println(stringValChar);
+
+
+
+    }
 }
