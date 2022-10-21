ec8bc42b2c00f5290d0e4499190630d96f4ec4bb
Ms-Yosa <yosa.jilliancr@gmail.com>
2022-10-21 09:02:58 +0800

    comparing string values

---
+++ src/E04.java
@@ -0,0 +1,44 @@
+public class E04 {
+
+    public static void main(String[] args){
+        String s1 ="Hello!";
+        var s2 = "Hello!";
+
+        //match
+        if(s1==s2){
+            System.out.println("They match!");
+        }else{
+            System.out.println("They don't match");
+        }
+
+        String s3 = new String("Hello!");
+        String s4 = new String("Hello!");
+        String s5 = new String("HELLO!");
+
+        // don't match
+        if(s3==s4){
+            System.out.println("They match!");
+        }else{
+            System.out.println("They don't match");
+        }
+
+        // matched!
+        if(s3.equals(s4)){
+            System.out.println("They match!");
+        }else{
+            System.out.println("They don't match");
+        }
+
+        // matched!
+        if(s3.equalsIgnoreCase(s5)){
+            System.out.println("They match!");
+        }else{
+            System.out.println("They don't match");
+        }
+
+
+
+
+
+    }
+}
