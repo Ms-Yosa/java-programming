614b6092166d7a3ebc99b72521e811a03cb0538f
Ms-Yosa <yosa.jilliancr@gmail.com>
2022-10-21 20:43:47 +0800

    create instance fields and methods

--- src/E19.java
+++ src/E19.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class E19 {
+import model.ClothingItem;
+
+import java.text.NumberFormat;
+
+public class E19 {
+    public static void main(String[] args){
+
+        var item = new ClothingItem();
+        item.setType("Shirt");
+        item.setPrice(19.99);
+        item.setSize("Small");
+        item.setQuantity(2);
+
+        var totalPrice = item.getPrice() * item.getQuantity();
+        var formatter = NumberFormat.getCurrencyInstance();
+        //output using the format
+        var output = String.format("Your %s order in size %s will have a total cost of %s",
+                item.getType(),
+                item.getSize(),
+                formatter.format(totalPrice));
+
+        System.out.println(output);
+    }
 }
