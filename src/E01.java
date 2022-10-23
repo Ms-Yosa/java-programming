f35bcca88b91b9315c62a4d4f6f555c91f45669d
Ms-Yosa <yosa.jilliancr@gmail.com>
2022-10-23 09:13:30 +0800

    initail commit

---
+++ src/E01.java
@@ -0,0 +1,34 @@
+public class E01 {
+    public static void main(String[] args){
+
+// variable review
+
+        String myName = "Todd";
+//        System.out.println(myName);
+
+//        method/function review
+//        printS(myName);
+
+//        Array review
+//        String[] items = new String[3];
+//        items[0] = "Clothes";
+//        items[1] = "Shoes";
+//        items[2] = "Pants";
+//
+//
+//        for (int i = 0; i < items.length; i++) {
+//            System.out.println(items[i]);
+//        }
+
+//      conditional statement review
+//       if (myName== "Todd"){
+//           System.out.println("true");
+//
+        }
+
+    }
+
+//    public static void printS(String data){
+//        System.out.println(data);
+//    }
+}
