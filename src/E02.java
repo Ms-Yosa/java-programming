ccdf314513c6ec926831c1c503af385acecbf068
Ms-Yosa <yosa.jilliancr@gmail.com>
2022-10-24 23:52:37 +0800

    review

--- src/E02.java
+++ src/E02.java
@@ -2,6 +2,9 @@ public class E02 {
     public static void main(String[] args){
 
 // review
-        System.out.println("I love pizza");
+        System.out.println("I love pizza\n");
+        System.out.println("\tI love pizza");
+        System.out.println("\"I love pizza\"");
+        System.out.println("\\I love pizza\\");
     }
 }
