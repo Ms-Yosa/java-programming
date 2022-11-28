81f4d31154077c551b44e14820723c93cac27960
Ms-Yosa <yosa.jilliancr@gmail.com>
2022-11-28 16:19:00 +0800

    escape sequence

---
+++ src/E06.java
@@ -0,0 +1,13 @@
+public class E06 {
+    public static void main (String[] args){
+        String message = "Hello \"Mosh\" ";
+        System.out.println(message);
+
+        String windows = "c:\\Windows\\...";
+        System.out.println(windows);
+
+        // \n = new line
+        // \t = tab
+
+    }
+}
