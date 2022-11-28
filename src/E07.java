e4c650e68651d6b4df5f9bfe1d056c525dc10d57
Ms-Yosa <yosa.jilliancr@gmail.com>
2022-11-28 16:25:29 +0800

    arrays

---
+++ src/E07.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+public class E07 {
+    public static void main(String[] args){
+        int[] numbers = {2,3,5,1,4};
+        System.out.println(numbers.length);
+        Arrays.sort(numbers);
+        System.out.println(Arrays.toString(numbers));
+
+
+    }
+}
