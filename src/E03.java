2e76594e18c1638df8e51c1368332b626d02f067
Ms-Yosa <yosa.jilliancr@gmail.com>
2022-11-28 12:28:12 +0800

    reference type

---
+++ src/E03.java
@@ -0,0 +1,14 @@
+import java.util.Date;
+
+public class E03 {
+    public static void main(String[] args){
+        // reference type = need packages
+        // declaring reference type needs to allocate a memory
+        // have members using the dot operator
+
+        byte age = 30;
+        Date now = new Date();
+        System.out.println(now);
+
+    }
+}
