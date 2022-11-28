359fe4bb3b53a0899abca5fdb20f81459504d86e
Ms-Yosa <yosa.jilliancr@gmail.com>
2022-11-28 16:33:48 +0800

    constant

---
+++ src/E09.java
@@ -0,0 +1,6 @@
+public class E09 {
+    public static void main(String[] args){
+        final float PI =3.14f; // use 'final' keyboard for constant variable
+        System.out.println(PI);
+    }
+}
