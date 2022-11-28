cad584e2cbee166dbc8f79a19cd66bc44f4499b1
Ms-Yosa <yosa.jilliancr@gmail.com>
2022-11-28 12:20:36 +0800

    primitive types

---
+++ src/E02.java
@@ -0,0 +1,14 @@
+public class E02 {
+    public static void main (String[] args){
+
+        // by default java see int and double so suffix is needed for declaring long and float variables
+        // orange words = reserved keywords
+
+        byte age = 30;
+        long viewsCount = 3_123_456_789L;
+        float price = 10.99f;
+        char letter = 'A';
+        boolean isEligible = true;
+
+    }
+}
