f90ab167f9f42ab1c4462b25c138593d00f91d13
Ms-Yosa <yosa.jilliancr@gmail.com>
2022-11-28 16:38:20 +0800

    arithmentic expression

---
+++ src/E10.java
@@ -0,0 +1,11 @@
+public class E10 {
+    public static void main(String[] args){
+        double result = 10/3;
+        int x =1;
+        // post/pre increment and decrement
+        //augmented/ compound assignment e.g. +=. -=, *= ...
+
+
+        System.out.println(result);
+    }
+}
